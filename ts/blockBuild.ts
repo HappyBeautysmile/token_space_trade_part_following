bc56f39bd3a266e8a0dc1803b306ac5866714491
DarkBear0121 <darkbear0112@gmail.com>
2022-04-05 21:03:48 -0700

    Cube Builder Basics?

---
+++ ts/blockBuild.ts
@@ -0,0 +1,89 @@
+import { getCipherInfo } from "crypto";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export class Hand extends THREE.Object3D {
+  constructor(grip: THREE.Object3D) {
+    super();
+    grip.add(this);
+
+    const cube = new THREE.Mesh(
+      new THREE.BoxBufferGeometry(0.1, 0.1, 0.1),
+      new THREE.MeshStandardMaterial({ color: '#987' }));
+    cube.position.z = -0.2;
+    this.add(cube);
+
+    const lineMaterial = new THREE.LineBasicMaterial({ color: '#def' });
+    const lineGeometry = new THREE.BufferGeometry()
+      .setFromPoints([new THREE.Vector3(), new THREE.Vector3(0, -0.5, 0)]);
+    const line = new THREE.Line(lineGeometry, lineMaterial);
+    this.add(line);
+
+    this.onBeforeRender = (renderer: THREE.WebGLRenderer,
+      scene: THREE.Scene, camera: THREE.Camera,
+      geometry: THREE.BufferGeometry, material: THREE.Material,
+      group: THREE.Group) => {
+    }
+
+    grip.addEventListener('selectstart', () => {
+      const o = cube.clone();
+      const p = o.position;
+      grip.getWorldPosition(p);
+      p.x = Math.round(p.x * 10) / 10;
+      p.y = Math.round(p.y * 10) / 10;
+      p.z = Math.round(p.z * 10) / 10;
+      grip.parent.add(o);
+    });
+
+    grip.addEventListener('selectend', () => {
+
+    });
+  }
+}
+
+export class BlockBuild {
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
+
+  private cubes = new Map<string, THREE.Object3D>();
+
+  constructor() {
+    this.setScene();
+    this.getGrips();
+  }
+
+  setScene() {
+    document.body.innerHTML = "";
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75,
+      window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    const light = new THREE.AmbientLight(0x404040); // soft white light
+    this.scene.add(light);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(40, 2, 10);
+    this.scene.add(directionalLight);
+
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    const render = () => {
+      requestAnimationFrame(render);
+      this.renderer.render(this.scene, this.camera);
+    }
+    render();
+  }
+
+  getGrips() {
+    for (const i of [0, 1]) {
+      const grip = this.renderer.xr.getControllerGrip(i);
+      this.scene.add(grip);
+    }
+  }
+}
---
+++ ts/exchangeDemo.ts
@@ -0,0 +1,57 @@
+import { BankAccount, BuyOrder, Exchange, SellOrder } from "./exchange";
+
+var time = 0;
+var exchange = new Exchange('iron');
+
+class Buyer {
+  private timeRemaining: number;
+  private bankAccount: BankAccount = new BankAccount(1000);
+  constructor(private buyIntervalS: number, private maxBid: number,
+    private exchange: Exchange) {
+    this.timeRemaining = buyIntervalS;
+  }
+  step(deltaS: number) {
+    this.timeRemaining -= deltaS;
+    if (this.timeRemaining < 0) {
+      this.timeRemaining += this.buyIntervalS;
+      const myBuyPrice = exchange.getHighestBuyPrice() + 1;
+      exchange.placeBuyOrder(new BuyOrder(
+        1, myBuyPrice, this.bankAccount));
+    }
+  }
+}
+
+var buyers: Buyer[] = [];
+
+for (let i = 0; i < 100; ++i) {
+  buyers.push(new Buyer(Math.random() * 10, Math.random() * 100, exchange));
+}
+
+const sellerAccount = new BankAccount(0);
+let factoryCount = 1;
+let inventory = 0;
+
+for (let step = 0; step < 20000; ++step) {
+  if (step % 10 === 0) {
+    inventory += factoryCount;
+  }
+  const demand = exchange.getHighestBuyPrice();
+  if (demand > 0) {
+    const numSold = exchange.sellWithLimit(
+      new SellOrder(inventory, 10, sellerAccount));
+    inventory -= numSold;
+  }
+  for (const b of buyers) {
+    b.step(0.1);
+  }
+  if (sellerAccount.getBalance() > 4000) {
+    ++factoryCount;
+    sellerAccount.removeFunds(4000);
+  }
+  if (step % 100 === 0) {
+    console.log(`Price: ${demand} balance: ${sellerAccount.getBalance()} inventory: ${inventory} factories: ${factoryCount}`);
+  }
+}
+
+
+
