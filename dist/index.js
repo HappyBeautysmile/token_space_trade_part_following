107f94fce09d8ab116688c6a3028d56226670ba6
DarkBear0121 <darkbear0112@gmail.com>
2022-04-17 19:24:09 -0700

    experiment

--- dist/index.js
+++ dist/index.js
@@ -78,15 +78,15 @@ class Hand extends THREE.Object3D {
         this.initialize();
     }
     tick(deltaS) {
-        this.cube.rotateX(0.1 * deltaS);
-        if (this.source) {
-            const rate = 0.1;
-            const axes = this.source.gamepad.axes.slice(0);
-            if (axes.length >= 4) {
-                this.cube.rotateX(axes[2] * rate * deltaS);
-                this.cube.rotateY(axes[3] * rate * deltaS);
-            }
-        }
+        this.cube.rotateX(0.1);
+        // if (this.source) {
+        //   const rate = 0.1;
+        //   const axes = this.source.gamepad.axes.slice(0);
+        //   if (axes.length >= 4) {
+        //     this.cube.rotateX(axes[2] * rate * deltaS);
+        //     this.cube.rotateY(axes[3] * rate * deltaS);
+        //   }
+        // }
     }
     setCube(o) {
         this.remove(this.cube);
--- ts/blockBuild.ts
+++ ts/blockBuild.ts
@@ -55,15 +55,15 @@ export class Hand extends THREE.Object3D {
   }
 
   public tick(deltaS: number) {
-    this.cube.rotateX(0.1 * deltaS);
-    if (this.source) {
-      const rate = 0.1;
-      const axes = this.source.gamepad.axes.slice(0);
-      if (axes.length >= 4) {
-        this.cube.rotateX(axes[2] * rate * deltaS);
-        this.cube.rotateY(axes[3] * rate * deltaS);
-      }
-    }
+    this.cube.rotateX(0.1);
+    // if (this.source) {
+    //   const rate = 0.1;
+    //   const axes = this.source.gamepad.axes.slice(0);
+    //   if (axes.length >= 4) {
+    //     this.cube.rotateX(axes[2] * rate * deltaS);
+    //     this.cube.rotateY(axes[3] * rate * deltaS);
+    //   }
+    // }
   }
 
   public setCube(o: THREE.Object3D) {
