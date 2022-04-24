068cff53a87c4dba3c9c6e32bf1237f823bc4e1f
DarkBear0121 <darkbear0112@gmail.com>
2022-04-24 09:39:53 -0700

    Update matt.js

--- dist/matt.js
+++ dist/matt.js
@@ -163,7 +163,7 @@ class BlockBuild {
         const debugPanel = new debug_1.Debug();
         debugPanel.position.set(0, 0, -3);
         this.universeGroup.add(debugPanel);
-        debug_1.Debug.log('comment out new Hand');
+        debug_1.Debug.log('comment out selectstart');
         // const controls = new OrbitControls(this.camera, this.renderer.domElement);
         // controls.target.set(0, 0, -5);
         // controls.update();
