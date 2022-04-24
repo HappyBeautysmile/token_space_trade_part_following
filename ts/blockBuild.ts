6e7d4fd4b66cfee95cd8f09e4504eb183304fa1c
DarkBear0121 <darkbear0112@gmail.com>
2022-04-24 12:45:35 -0700

    restored if(source)

--- ts/blockBuild.ts
+++ ts/blockBuild.ts
@@ -151,7 +151,7 @@ export class BlockBuild {
     const debugPanel = new Debug();
     debugPanel.position.set(0, 0, -3);
     this.universeGroup.add(debugPanel);
-    Debug.log("restored constrocor without grip.add(this);");
+    Debug.log("restored if(source)");
 
     // const controls = new OrbitControls(this.camera, this.renderer.domElement);
     // controls.target.set(0, 0, -5);
