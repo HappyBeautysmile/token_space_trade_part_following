57d392779b297c7c314e4105b86624c951ab983f
DarkBear0121 <darkbear0112@gmail.com>
2022-04-25 15:27:49 -0700

    Created assets class

--- ts/assets.ts
+++ ts/assets.ts
@@ -4,15 +4,17 @@ import { Palette } from "./palette";
 
 export class Assets extends THREE.Object3D {
 
-    private palette = new Palette();
+    static init() {
+        Palette.init();
+    }
 
     // sets the color of the passed object to the next color in the palette.
-    public nextColor(source: THREE.Object3D) {
-        const newMat = new THREE.MeshPhongMaterial({ color: this.palette.nextColor() });
+    static nextColor(source: THREE.Object3D) {
+        const newMat = new THREE.MeshPhongMaterial({ color: Palette.nextColor() });
         this.replaceMaterial(source, newMat);
     }
 
-    public replaceMaterial(source: THREE.Object3D, mat: THREE.Material) {
+    static replaceMaterial(source: THREE.Object3D, mat: THREE.Material) {
         console.log(`${source.name} (${source.type})`);
         for (let i = 0; i < source.children.length; i++) {
             let mesh = source.children[i] as THREE.Mesh;
