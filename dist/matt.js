2ff7c5969c0f8d0ced3d74cf9c744776809c3f17
DarkBear0121 <darkbear0112@gmail.com>
2022-04-29 13:28:45 -0700

    White

--- dist/matt.js
+++ dist/matt.js
@@ -1833,7 +1833,7 @@ class VeryLargeUniverse extends THREE.Object3D {
         uniform float sizeScale;
         varying vec3 vColor;
         void main() {
-          vColor = vec3(0.5, 1.0, 1.0);
+          vColor = vec3(1.0, 1.0, 1.0);
           vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
           float distance = length(mvPosition.xyz);
           if (distance > 1000.0) {
