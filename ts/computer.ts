f19f4bbe95584b10dc83edc29ebecec7ab6049e7
DarkBear0121 <darkbear0112@gmail.com>
2022-05-14 08:52:08 -0700

    Create computer.ts

---
+++ ts/computer.ts
@@ -0,0 +1,9 @@
+export class Computer {
+    private ctx = document.createElement("CANVAS");
+
+    createGreenBars() {
+        // this.ctx.moveTo(0, 0);
+        // this.ctx.lineTo(200, 100);
+        // this.ctx.stroke();
+    }
+}
