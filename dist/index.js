98d5480207261cb2c876c314e1ceb78f9331c74b
DarkBear0121 <darkbear0112@gmail.com>
2022-05-29 13:38:32 -0700

    sounds working.

--- dist/index.js
+++ dist/index.js
@@ -688,7 +688,7 @@ class BlockBuild {
             const audioLoader = new THREE.AudioLoader();
             const num = Math.ceil(Math.random() * 5).toFixed(0);
             const soundname = `sounds/mine${num}.ogg`;
-            audioLoader.load(soundname, function (buffer) {
+            audioLoader.load(soundname, (buffer) => {
                 sound.setBuffer(buffer);
                 sound.setLoop(false);
                 sound.setVolume(0.5);
@@ -702,18 +702,6 @@ class BlockBuild {
         document.body.innerHTML = "";
         this.scene.add(this.playerGroup);
         this.scene.add(this.universeGroup);
-        //this.scene.background = new THREE.Color(0x552200);
-        // var skyGeo = new THREE.SphereGeometry(1900, 25, 25);
-        // var loader = new THREE.TextureLoader()
-        // var texture = loader.load("Model/sky6.jpg");
-        // var color = new THREE.Color(Math.random(), Math.random(), Math.random())
-        // var material = new THREE.MeshBasicMaterial({
-        //   map: texture,
-        //   color: color
-        // });
-        // var sky = new THREE.Mesh(skyGeo, material);
-        // sky.material.side = THREE.BackSide;
-        // this.universeGroup.add(sky);
         const sky = new skyBox_1.SkyBox();
         this.universeGroup.add(sky);
         this.camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 2000);
@@ -769,7 +757,7 @@ class BlockBuild {
         //   sound.play();
         // });
         debug_1.Debug.log("Three Version=" + THREE.REVISION);
-        debug_1.Debug.log("sound when placed 2");
+        debug_1.Debug.log("sound when placed 3");
         // const controls = new OrbitControls(this.camera, this.renderer.domElement);
         // controls.target.set(0, 0, -5);
         // controls.update();
@@ -2115,6 +2103,15 @@ class Hand extends THREE.Object3D {
         const removedCube = this.construction.removeCube(this.p);
         if (removedCube) {
             this.inventory.addItem(removedCube);
+            const num = Math.ceil(Math.random() * 5).toFixed(0);
+            const soundname = `sounds/mine${num}.ogg`;
+            debug_1.Debug.log(`playing sound ${soundname}`);
+            this.audioLoader.load(soundname, (buffer) => {
+                this.sound.setBuffer(buffer);
+                this.sound.setLoop(false);
+                this.sound.setVolume(0.5);
+                this.sound.play();
+            });
         }
     }
     eulerString(q) {
@@ -2152,8 +2149,9 @@ class Hand extends THREE.Object3D {
                         this.setCube(assets_1.Assets.itemsByName.get('guide'));
                     }
                     const num = Math.ceil(Math.random() * 5).toFixed(0);
-                    const soundname = `sounds/mine${num}.ogg`;
-                    this.audioLoader.load(soundname, function (buffer) {
+                    const soundname = `sounds/build${num}.ogg`;
+                    debug_1.Debug.log(`playing sound ${soundname}`);
+                    this.audioLoader.load(soundname, (buffer) => {
                         this.sound.setBuffer(buffer);
                         this.sound.setLoop(false);
                         this.sound.setVolume(0.5);
