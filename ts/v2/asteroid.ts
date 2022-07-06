610c0e0ab550bcc613921dbefc85edd1e9293cbe
DarkBear0121 <darkbear0112@gmail.com>
2022-07-05 20:08:58 -0700

    Build the actual asteroids.

--- ts/v2/asteroid.ts
+++ ts/v2/asteroid.ts
@@ -1,26 +1,18 @@
 import * as THREE from "three";
 import { S } from "../settings";
 import { Assets } from "./assets";
+import { AstroGen } from "./astroGen";
 
 import { Codeable } from "./file";
-import { Grid } from "./grid";
 import { MeshCollection } from "./meshCollection";
 
 export class Asteroid extends MeshCollection implements Codeable {
   constructor(assets: Assets) {
     super(assets);
   }
-
-  private gridPosition() {
-    return Math.round((Math.random() - 0.5) * 200);
-  }
-
   fallback(p: THREE.Vector3) {
-    for (let i = 0; i < S.float('ni'); ++i) {
-      const pos = new THREE.Vector3(
-        this.gridPosition(), this.gridPosition(), this.gridPosition());
-      this.addCube('cube', pos, Grid.notRotated);
-    }
+    const gen = new AstroGen(this);
+    gen.buildAsteroid(5, 0, 0, 0);
     this.buildGeometry();
     return this;
   }
