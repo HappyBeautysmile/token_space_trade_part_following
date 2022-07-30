628cf2ff401ff8116252c65427ae13c98e6a5c62
DarkBear0121 <darkbear0112@gmail.com>
2022-07-30 14:23:34 -0700

    Add iterator to latice

--- ts/v2/latice.ts
+++ ts/v2/latice.ts
@@ -1,8 +1,14 @@
 import * as THREE from "three";
 
+export class LaticeEntry<T> {
+  constructor(readonly position: THREE.Vector3, readonly value: T) { }
+}
+
 export class Latice<T> {
   private keyToCode = new Map<T, number>();
   private codeToKey = new Map<number, T>();
+  private keyCount = new Map<T, number>();
+
   readonly origin = new THREE.Vector3();
   private data: Uint8Array;
 
@@ -34,14 +40,36 @@ export class Latice<T> {
 
   Set(pos: THREE.Vector3, value: T) {
     let code: number;
+    const index = this.getIndexFromVector(pos);
+    if (this.data[index] > 0) {
+      // TODO: decrement counter.
+    }
     if (!this.keyToCode.has(value)) {
       code = this.codeToKey.size;
       this.keyToCode.set(value, code);
       this.codeToKey.set(code, value);
+      this.keyCount.set(value, 1);
     } else {
       code = this.keyToCode.get(value);
+      this.keyCount.set(value, this.keyCount.get(value) + 1);
     }
-    const index = this.getIndexFromVector(pos);
     this.data[index] = code;
+  }
+
+  * Entries(): Iterable<LaticeEntry<T>> {
+    for (let z = 0; z < this.edgeSize; ++z) {
+      for (let y = 0; y < this.edgeSize; ++y) {
+        let index = this.getIndex(0, y, z);
+        for (let x = 0; x < this.edgeSize; ++x) {
+          const code = this.data[index];
+          if (code != 0) {
+            const pos = new THREE.Vector3(x, y, z);
+            pos.add(this.origin);
+            yield new LaticeEntry(pos, this.codeToKey.get(code));
+          }
+          ++index;
+        }
+      }
+    }
   }
 }
