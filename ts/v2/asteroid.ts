3b98d467c8c966856b1e5326c3ecfc0382caae32
DarkBear0121 <darkbear0112@gmail.com>
2022-08-07 15:00:05 -0700

    Combine with previous

--- ts/v2/asteroid.ts
+++ ts/v2/asteroid.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { S } from "../settings";
 import { Assets } from "./assets";
 import { AstroGen } from "./astroGen";
+import { Compounds } from "./compounds";
 import { Controls, StartStopEvent, StartStopEventHandler } from "./controls";
 import { Cursor } from "./cursor";
 
@@ -24,18 +25,32 @@ export class Asteroid extends MeshCollection implements Codeable {
 
         const cursor = cursors.get(ev.handedness);
         if (cursor.isHolding()) {
-          if (!this.cubeAt(pos.position)) {
-            this.addCube(cursor.getHold(), pos);
-            cursor.setHold(null);
-          }
+          this.handleDrop(pos, cursor);
         } else {
           const removed = this.removeCube(pos.position);
           cursor.setHold(removed);
         }
       }
     });
-
   }
+
+  private compounds = new Compounds();
+
+  private handleDrop(pos: IsoTransform, cursor: Cursor) {
+    if (!this.cubeAt(pos.position)) {
+      this.addCube(cursor.getHold(), pos);
+      cursor.setHold(null);
+    } else {
+      const existingCube = this.get(pos.position);
+      const combo = this.compounds.combine(existingCube, cursor.getHold());
+      if (!!combo) {
+        this.removeCube(pos.position);
+        this.addCube(combo, pos);
+        cursor.setHold(null);
+      }
+    }
+  }
+
   fallback(p: THREE.Vector3) {
     const gen = new AstroGen(this);
     gen.buildAsteroid(S.float('as'), 0, 0, 0);
